feaaa670e8569406a4fd680f30daecd01ea54bee
Elinda T. <elinda.tragel@gmail.com>
2024-12-03 14:33:09 +0200

    Win Conditions and Taking Turns

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,9 +8,12 @@ public class GameController : MonoBehaviour
 {
     public List<TMP_Text> buttonList;
 
+    private string playerSide;
+
     void Start()
     {
         SetGameControllerReferenceOnButtons();
+        playerSide = "X";
     }
 
     void SetGameControllerReferenceOnButtons()
@@ -23,11 +26,72 @@ public class GameController : MonoBehaviour
 
     public string GetPlayerSide()
     {
-        return "?";
+        return playerSide;
     }
 
     public void EndTurn()
     {
-        Debug.Log("EndTurn is not implemented!");
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        ChangeSides();
+
+    }
+
+    void ChangeSides()
+    {
+        if (playerSide == "X")
+        {
+            playerSide = "O";
+        }
+        else
+        {
+            playerSide = "X";
+        }
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
     }
 }
