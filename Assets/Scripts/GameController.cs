0b7f0c0e6d6d9d5d42d656d0e61a88ea32700f9e
Elinda T. <elinda.tragel@gmail.com>
2024-12-03 14:54:58 +0200

    Ending in a Draw

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,12 +11,14 @@ public class GameController : MonoBehaviour
     public TMP_Text gameOverText;
 
     private string playerSide;
+    private int moveCount;
 
     void Start()
     {
         SetGameControllerReferenceOnButtons();
         playerSide = "X";
         gameOverPanel.SetActive(false);
+        moveCount = 0;
     }
 
     void SetGameControllerReferenceOnButtons()
@@ -34,6 +36,8 @@ public class GameController : MonoBehaviour
 
     public void EndTurn()
     {
+        moveCount++;
+
         if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
         {
             GameOver();
@@ -74,6 +78,11 @@ public class GameController : MonoBehaviour
             GameOver();
         }
 
+        if (moveCount >= 9)
+        {
+            SetGameOverText("It's a draw!");
+        }
+
         ChangeSides();
     }
 
@@ -96,7 +105,12 @@ public class GameController : MonoBehaviour
             buttonList[i].GetComponentInParent<Button>().interactable = false;
         }
 
+        SetGameOverText(playerSide + " Wins!");
+    }
+
+    void SetGameOverText(string value)
+    {
         gameOverPanel.SetActive(true);
-        gameOverText.text = playerSide + " Wins!";
+        gameOverText.text = value;
     }
 }
